9ef54e6446316baa8d15e70b4dcd95210cd6206a
Illia <77698519+illia-fz@users.noreply.github.com>
2025-09-28 23:29:12 -0700

    Add SimpleStorage.sol contract example

---
+++ SimpleStorage.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    event DataStored(uint256 data);
+
+    function set(uint256 x) public {
+        storedData = x;
+        emit DataStored(x);
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
